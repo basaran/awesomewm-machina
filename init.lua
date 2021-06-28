ed99bf7b05516764941752b76b8e727a9ac195e8
anakha <basaran@protonmail.com>
2021-06-28 05:14:36 -0400

    bug fix

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@
 local capi = {root=root}
 local gears = require("gears")
 local naughty = require("naughty")
-local inspect = require('inspect')
+-- local inspect = require('inspect')
 local awful = require("awful")
 local modkey = "Mod4"
 
